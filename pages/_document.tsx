82d85c006d48d907aac2a6894074db2d346e9acf
maf21 <90659530+maf21@users.noreply.github.com>
2022-08-09 10:40:32 -0500

    corregir build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
-/*Configuraciones globales de la app*/
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script src="https://kit.fontawesome.com/afd31a68b2.js" crossOrigin="anonymous"></script>
+        <Script src="https://kit.fontawesome.com/afd31a68b2.js" crossOrigin="anonymous"></Script>
       </Head>
       <body>
         <Main />
