f71e4fd7d8717a8125662ec72a3f44848f827fe2
maf21 <90659530+maf21@users.noreply.github.com>
2022-08-08 19:05:19 -0500

    despliegue realizado y ejecutando prueba de link a home desde la pag  hola

--- pages/hola.tsx
+++ pages/hola.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const hola = () => {
   return (
     <div>
         hola
+        <Link href='/'>
+        <a>ir a pag. home</a>
+        </Link>
     </div>
   )
 }
